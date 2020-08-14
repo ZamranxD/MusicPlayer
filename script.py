5f2aaf1e0fe31fa73934fc3cbd47f74d5c74ba4c
ZamranxD <zamran.butt.1@gmail.com>
2020-08-14 21:56:06 +0500

    adding some sample text and an image for a play button

--- script.py
+++ script.py
@@ -5,4 +5,11 @@ window = Tk()
 window.geometry('300x300')
 window.title("Newzic Player")
 
+text = Label(window, text="Sample text I guess..")
+text.pack()
+
+photo = PhotoImage(file='play-button.png')
+photolabel = Label(window, image=photo)
+photolabel.pack()
+
 window.mainloop()
