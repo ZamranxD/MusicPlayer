023b1700d439c7743845bd3b92f696c06ffb4a0b
ZamranxD <zamran.butt.1@gmail.com>
2020-08-16 13:31:55 +0500

    adding volume control widget

--- script.py
+++ script.py
@@ -8,13 +8,17 @@ window.title("Newzic Player")
 
 mixer.init() #initializing pygame mixer
 
-#functions for the start and stop buttons
+#functions for the start and stop buttons and the volume control
 def play_music():
     mixer.music.load("nani.mp3")
     mixer.music.play()
 
 def stop_music():
     mixer.music.stop()
+
+def volume_control(val):
+    volume = int(val) / 100
+    mixer.music.set_volume(volume)
 
 
 text = Label(window, text="Sample text I guess..")
@@ -28,6 +32,11 @@ play_btn.pack()
 stop_photo = PhotoImage(file='stop.png')
 stop_btn = Button(window, image=stop_photo, command=stop_music)
 stop_btn.pack()
+
+#adding a scale widget for volume control
+scale = Scale(window, from_=0, to=100, orient = HORIZONTAL, command=volume_control)
+scale.set(75)
+scale.pack()
 
 
 
