b71d74715bc6f697cc59fd03bce244e57425cb70
ZamranxD <zamran.butt.1@gmail.com>
2020-08-18 18:29:49 +0500

    add pause/resume functionality

--- script.py
+++ script.py
@@ -3,7 +3,7 @@ import tkinter.messagebox
 from pygame import mixer
 import tkinter.filedialog
 import os
-
+import pause
 
 #functions for the toolbar
 def about_us():
@@ -16,16 +16,22 @@ def browse():
     global filename
     filename = tkinter.filedialog.askopenfilename()
 
-#functions for the start and stop buttons and the volume control
+#functions for the play, pause and stop buttons and the volume control
 def play_music():
-    try:
-        mixer.music.load(filename)
-        mixer.music.play()
-        status_bar['text'] = f"Playing {os.path.basename(filename)}"
-    except:
-        tkinter.messagebox.showerror("No file selected", "Please make sure to open a file first before clicking play.")
+    if pause.paused == False:
+        try:
+            mixer.music.load(filename)
+            mixer.music.play()
+            status_bar['text'] = f"Playing music - {os.path.basename(filename)}"
+        except:
+            tkinter.messagebox.showerror("No file selected", "Please make sure to open a file first before clicking play.")
+    elif pause.paused==True:
+        mixer.music.unpause()
+        status_bar['text'] = f"Playing music - {os.path.basename(filename)}"
+
 
 def stop_music():
+    pause.paused = False
     mixer.music.stop()
     status_bar['text'] = f"{os.path.basename(filename)} Stopped"
 
@@ -33,6 +39,11 @@ def stop_music():
 def volume_control(val):
     volume = int(val) / 100
     mixer.music.set_volume(volume)
+
+def pause_music():
+    pause.paused = True
+    mixer.music.pause()
+    status_bar['text'] = "Music paused"
 
 
 
@@ -62,7 +73,7 @@ mixer.init() #initializing pygame mixer for volume control
 text = Label(window, text="Sample text I guess..")
 text.pack()
 
-#start and stop buttons
+#play, pause and stop buttons
 start_photo = PhotoImage(file='play.png')
 play_btn = Button(window, image=start_photo, command=play_music)
 play_btn.pack()
@@ -70,6 +81,10 @@ play_btn.pack()
 stop_photo = PhotoImage(file='stop.png')
 stop_btn = Button(window, image=stop_photo, command=stop_music)
 stop_btn.pack()
+
+pause_photo = PhotoImage(file='pause.png')
+pause_btn = Button(window, image=pause_photo, command=pause_music)
+pause_btn.pack()
 
 #adding a scale widget for volume control
 scale = Scale(window, from_=0, to=100, orient = HORIZONTAL, command=volume_control)
